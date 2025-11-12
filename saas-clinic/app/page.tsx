98632029986cf5c61b6b561f862e8d526c891133
afnan <afnan232003@gmail.com>
2025-11-12 13:54:34 +0200

    Add Login Page

--- saas-clinic/app/page.tsx
+++ saas-clinic/app/page.tsx
@@ -1,7 +1,9 @@
+import Login from "../src/Pages/Login";
 const Home =() =>{
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 <h1>Saas For Clinics</h1>
+<Login />
     </div>
   );
 }
